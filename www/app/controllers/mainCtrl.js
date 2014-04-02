1c834d3aa17b1c7797617856b7a97eb84353cb39
Kristoffer Øseth <kristoffer.oseth@gmail.com>
2014-04-01 21:43:04 -0700

    debug scroll

--- www/app/controllers/mainCtrl.js
+++ www/app/controllers/mainCtrl.js
@@ -70,6 +70,26 @@ function mainCtrl($scope, $location, stateService, learnFactory) {
     var dragCounter = 0;
     var newParent;
 
+    var lockScroll = function () {
+        var scrollPosition = [
+                self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
+                self.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
+        ];
+        var html = jQuery('.listOfWords'); // it would make more sense to apply this to body, but IE7 won't have that
+        html.data('scroll-position', scrollPosition);
+        html.data('previous-overflow', html.css('overflow'));
+        html.css('overflow', 'hidden');
+        window.scrollTo(scrollPosition[0], scrollPosition[1]);
+
+    }
+
+    var unlockScroll = function() {
+        var html = jQuery('listOfWords');
+        var scrollPosition = html.data('scroll-position');
+        html.css('overflow', html.data('previous-overflow'));
+        window.scrollTo(scrollPosition[0], scrollPosition[1])
+    }
+
     $scope.setDraggable = function() {
         $(".draggable").draggable({
             drag: function(even,ui) {
@@ -95,6 +115,8 @@ function mainCtrl($scope, $location, stateService, learnFactory) {
             }
         });
     }
+
+
 
     $scope.setDroppable = function() {
         $(".inputBox").droppable({
